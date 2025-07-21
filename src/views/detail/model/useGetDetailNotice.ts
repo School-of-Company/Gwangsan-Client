c11e9361571e6dbf463f25a1f1f99a5ee5f2be49
bae080311 <s24024@gsm.hs.kr>
2025-07-22 00:02:57 +0900

    fix: type

--- src/views/detail/model/useGetDetailNotice.ts
+++ src/views/detail/model/useGetDetailNotice.ts
@@ -1,19 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { getDetailNotice } from '../api/getDetailNotice';
-import { Notice } from '@/widgets/notice/model/noticeSchema';
-import { Place } from '@/shared/const/place';
-import { MemberRole } from '@/shared/const/role';
-import { ImageType } from '@/shared/types/imageType';
-
-interface DetailResponse extends Notice {
-  place: Place;
-  createdAt: string;
-  role: MemberRole;
-  images: ImageType[];
-}
+import { DetailNotice } from '@/shared/types/noticeType';
 
 export const useGetDetailNotice = (id: string) => {
-  return useQuery<DetailResponse>({
+  return useQuery<DetailNotice>({
     queryKey: ['detailNotice', id],
     queryFn: () => getDetailNotice(id),
   });
--- src/widgets/notice/model/useGetNotices.ts
+++ src/widgets/notice/model/useGetNotices.ts
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { getNotices } from '../api/getNotices';
+import { Notices } from '@/shared/types/noticeType';
 
-export const useGetNotice = () => {
-  return useQuery({
+export const useGetNotices = () => {
+  return useQuery<Notices>({
     queryKey: ['notice'],
     queryFn: getNotices,
   });
