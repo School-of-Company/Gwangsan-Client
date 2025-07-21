2abc5ae3f52f6caa5048cfacbb1619d5111c822d
bae080311 <s24024@gsm.hs.kr>
2025-07-22 00:40:00 +0900

    fix: notice style

--- src/widgets/notice/ui/PostedNotice/index.tsx
+++ src/widgets/notice/ui/PostedNotice/index.tsx
@@ -7,18 +7,24 @@ import { NoticeCard } from '@/entities/notice';
 export default function PostedNotice() {
   const { data } = useGetNotices();
   return (
-    <div className={cn('w-full')}>
+    <div className={cn('w-full px-11')}>
       <h2 className={cn('text-titleMedium2')}>게시된 공지사항</h2>
-      {data?.map((v) => {
-        return (
-          <NoticeCard
-            title={v.title}
-            content={v.content}
-            key={v.id}
-            id={v.id}
-          />
-        );
-      })}
+      <div
+        className={cn(
+          'mt-[28px] flex h-[100vh] flex-col gap-3 overflow-y-visible',
+        )}
+      >
+        {data?.map((v) => {
+          return (
+            <NoticeCard
+              title={v.title}
+              content={v.content}
+              key={v.id}
+              id={v.id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
