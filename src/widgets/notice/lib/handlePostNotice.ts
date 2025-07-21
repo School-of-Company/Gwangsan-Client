f76e3695031942efd4ffd72cca002cac6aecb9ac
bae080311 <s24024@gsm.hs.kr>
2025-07-21 13:44:35 +0900

    fixl: handle notice

--- src/widgets/notice/lib/handlePostNotice.ts
+++ src/widgets/notice/lib/handlePostNotice.ts
@@ -1,58 +1,44 @@
 import { Place } from '@/shared/const/place';
 import { MemberRole } from '@/shared/const/role';
-import { storage } from '@/shared/lib/storage';
 import { NoticeSchema } from '../model/noticeSchema';
 import { postNotice } from '../api/postNotice';
 import { toast } from 'sonner';
+import { uploadImage } from '../api/uploadImage';
 
-type NoticeError = {
-  title?: string[];
-  content?: string[];
-  placeName?: string[];
-  imageIds?: string[];
-};
+export const handlePostNotice = async (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
 
-type NoticeState = {
-  success: boolean;
-  error: NoticeError;
-};
+  const formData = new FormData(e.currentTarget);
+  const imageFile = await uploadImage([formData.get('imageIds') as File]);
 
-export const handlePostNotice = async (
-  state: NoticeState,
-  formData: FormData,
-): Promise<NoticeState> => {
   const value = {
     title: formData.get('title') as string,
     content: formData.get('content') as string,
     placeName: formData.get('placeName') as Place,
-    imageIds: formData.get('imageIds') as File,
-    role: storage.getItem('role') as MemberRole,
+    imageIds: imageFile,
+    role: formData.get('role') as MemberRole,
   };
 
   const result = NoticeSchema.safeParse(value);
   if (result.success) {
     const res = await postNotice(value);
     if (res) {
-      if (res.status === 201) toast.success('공지가 작성되었습니다');
-      else
+      if (res.status === 201) {
+        toast.success('공지가 작성되었습니다');
+        e.currentTarget.reset();
+      } else {
         toast.error(
-          res?.status === 403
+          res.status === 403
             ? '공지 작성 권한이 없는 지역입니다'
             : '공지 작성 실패했습니다',
         );
+      }
     }
-    return { ...state, success: true, error: {} };
   } else {
     const fieldErrors = result.error.flatten().fieldErrors;
-    return {
-      ...state,
-      success: false,
-      error: {
-        title: fieldErrors.title,
-        content: fieldErrors.content,
-        placeName: fieldErrors.placeName,
-        imageIds: fieldErrors.imageIds,
-      },
-    };
+    toast.error(fieldErrors.content);
+    toast.error(fieldErrors.imageIds);
+    toast.error(fieldErrors.placeName);
+    toast.error(fieldErrors.title);
   }
 };
