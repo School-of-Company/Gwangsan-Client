1e61e9c19a5a9975a366bd0b99b3a963e1eb4944
bae080311 <s24024@gsm.hs.kr>
2025-07-22 00:06:51 +0900

    feat: map data

--- src/widgets/notice/ui/PostedNotice/index.tsx
+++ src/widgets/notice/ui/PostedNotice/index.tsx
@@ -1,9 +1,24 @@
+'use client';
+
 import { cn } from '@/shared/lib/utils';
+import { useGetNotices } from '../../model/useGetNotices';
+import { NoticeCard } from '@/entities/notice';
 
 export default function PostedNotice() {
+  const { data } = useGetNotices();
   return (
     <div className={cn('w-full')}>
       <h2 className={cn('text-titleMedium2')}>게시된 공지사항</h2>
+      {data?.map((v) => {
+        return (
+          <NoticeCard
+            title={v.title}
+            content={v.content}
+            key={v.id}
+            id={v.id}
+          />
+        );
+      })}
     </div>
   );
 }
