31b94f80057432176cea7ef461f7f55984b57320
bae080311 <s24024@gsm.hs.kr>
2025-07-21 14:06:55 +0900

    feat: show notice

---
+++ src/widgets/notice/ui/PostedNotice/index.tsx
@@ -0,0 +1,9 @@
+import { cn } from '@/shared/lib/utils';
+
+export default function PostedNotice() {
+  return (
+    <div className={cn('w-full')}>
+      <h2 className={cn('text-titleMedium2')}>게시된 공지사항</h2>
+    </div>
+  );
+}
