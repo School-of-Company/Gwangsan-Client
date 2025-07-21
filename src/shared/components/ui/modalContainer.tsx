c49e9f56dcfd62c8ad9eb43c3c5913c62e9a7a72
bae080311 <s24024@gsm.hs.kr>
2025-07-22 07:04:50 +0900

    feat: modal container

---
+++ src/shared/components/ui/modalContainer.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@/shared/lib/utils';
+import { Close } from '@radix-ui/react-dialog';
+import React from 'react';
+
+interface ModalContainerProps {
+  children: React.ReactNode;
+  className: string;
+}
+
+export default function ModalContainer({
+  children,
+  className,
+}: ModalContainerProps) {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(17,17,17,0.6)]">
+      <div
+        className={cn(
+          'w-full max-w-lg rounded-xl bg-white p-6 shadow-xl',
+          className,
+        )}
+      >
+        <Close className={cn('ml-auto')} />
+        {children}
+      </div>
+    </div>
+  );
+}
