8d0a97906634cfcfd704183c742797cb32f445ce
bae080311 <s24024@gsm.hs.kr>
2025-07-22 10:27:36 +0900

    fix: key

--- src/shared/api/changeStatus.ts
+++ src/shared/api/changeStatus.ts
@@ -3,13 +3,15 @@ import { instance } from '../lib/axios';
 import { MEMBER_STATUS } from '../types/memberType';
 import { toast } from 'sonner';
 
-export const changeStatus = (id: string, MEMBER_STATUS: MEMBER_STATUS) => {
+export const changeStatus = async (
+  id: string,
+  MEMBER_STATUS: MEMBER_STATUS,
+) => {
   try {
-    const res = instance.patch('/admin/status/' + id, {
-      MEMBER_STATUS,
+    const res = await instance.patch('/admin/status/' + id, {
+      status: MEMBER_STATUS,
     });
-    toast.success('상태 변경에 성공했습니다');
-    return res;
+    if (res.status === 204) toast.success('상태 변경에 성공했습니다');
   } catch (error) {
     toast.error('상태 변경에 실패했습니다');
     throw error;
