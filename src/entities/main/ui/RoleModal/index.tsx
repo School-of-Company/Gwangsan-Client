b1d42cce493b4e2354499e01a476818ebf1d7ea6
bae080311 <s24024@gsm.hs.kr>
2025-07-22 17:31:33 +0900

    fix: dialog

--- src/entities/main/ui/RoleModal/index.tsx
+++ src/entities/main/ui/RoleModal/index.tsx
@@ -3,10 +3,9 @@
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogDescription,
-  DialogOverlay,
 } from '@/shared/components/ui/dialog';
 import { Label } from '@/shared/components/ui/label';
 import { Button } from '@/shared/components/ui/button';
@@ -17,53 +16,71 @@ import {
   SelectItem,
   SelectValue,
 } from '@/shared/components/ui/select';
+import { useState } from 'react';
+import { changeRole } from '@/widgets/main/api/changeRole';
+import { MemberRole, memberRoleOptions } from '@/shared/const/role';
 
 interface EditRoleDialogProps {
   open: boolean;
-  userName: string;
   setShow: (v: boolean) => void;
+  id: string;
+  name: string;
 }
 
-const roleOptions = ['코디네이터', '관리자', '운영진'];
-
 export default function RoleModal({
-  userName,
   open,
   setShow,
+  name,
+  id,
 }: EditRoleDialogProps) {
+  const [selectedRole, setSelectedRole] = useState<MemberRole | undefined>(
+    undefined,
+  );
+
+  const handleSave = () => {
+    if (!selectedRole) return;
+    changeRole(id, selectedRole);
+    setShow(false);
+  };
+
   return (
-    <div className="bg-[rgba(17, 17, 17, 0.60) fixed inset-0 z-40">
-      <Dialog open={open} onOpenChange={setShow}>
-        <DialogContent className="z-50 rounded-xl bg-white sm:max-w-[400px] dark:bg-white">
-          <DialogHeader>
-            <DialogTitle className="text-lg">역할 수정</DialogTitle>
-            <DialogDescription>
-              <span className="font-medium text-black">{userName}</span>님의
-              역할을 수정합니다
-            </DialogDescription>
-          </DialogHeader>
+    <Dialog open={open} onOpenChange={setShow}>
+      <DialogContent className="z-50 rounded-xl bg-white sm:max-w-[400px]">
+        <DialogHeader>
+          <DialogTitle className="text-lg">역할 수정</DialogTitle>
+          <DialogDescription>
+            {name + '님의 역할을 수정합니다'}
+          </DialogDescription>
+        </DialogHeader>
 
-          <div className="space-y-2">
-            <Label>역할</Label>
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="역할을 선택하세요" />
-              </SelectTrigger>
-              <SelectContent>
-                {roleOptions.map((r) => (
-                  <SelectItem key={r} value={r}>
-                    {r}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div>
+        <div className="space-y-2">
+          <Label>역할</Label>
+          <Select
+            value={selectedRole}
+            onValueChange={(e) => setSelectedRole(e as MemberRole)}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="역할을 선택하세요" />
+            </SelectTrigger>
+            <SelectContent className="bg-white">
+              {memberRoleOptions.map((r) => (
+                <SelectItem key={r.value} value={r.value}>
+                  {r.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
 
-          <Button className="mt-6 w-full" variant="outline">
-            저장하기
-          </Button>
-        </DialogContent>
-      </Dialog>
-    </div>
+        <Button
+          onClick={handleSave}
+          className="mt-6 w-full"
+          variant="outline"
+          disabled={!selectedRole}
+        >
+          저장하기
+        </Button>
+      </DialogContent>
+    </Dialog>
   );
 }
