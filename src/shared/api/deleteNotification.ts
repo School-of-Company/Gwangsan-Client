89a4438b9c8ebc3d385b3260e96d767c8d7b8592
bae080311 <s24024@gsm.hs.kr>
2025-07-23 10:25:01 +0900

    feat: delete request

---
+++ src/shared/api/deleteNotification.ts
@@ -0,0 +1,11 @@
+import { toast } from 'sonner';
+import { instance } from '../lib/axios';
+
+export const deleteNotification = async (id: string) => {
+  try {
+    await instance.delete('/api/admin/' + id);
+  } catch (error) {
+    toast.error('알림 삭제 실패했습니다');
+    throw error;
+  }
+};
