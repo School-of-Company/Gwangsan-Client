191ac2d3fc2e1c9451a7af37f05084c9c0956797
bae080311 <s24024@gsm.hs.kr>
2025-07-23 11:16:12 +0900

    remove: /api

--- src/shared/api/deleteNotification.ts
+++ src/shared/api/deleteNotification.ts
@@ -3,7 +3,7 @@ import { instance } from '../lib/axios';
 
 export const deleteNotification = async (id: string) => {
   try {
-    await instance.delete('/api/admin/' + id);
+    await instance.delete('/admin/' + id);
   } catch (error) {
     toast.error('알림 삭제 실패했습니다');
     throw error;
