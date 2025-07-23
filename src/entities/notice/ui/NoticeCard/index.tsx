52f862c41f9baf57530841c0f72a7af3b86d3961
bae080311 <s24024@gsm.hs.kr>
2025-07-23 11:23:30 +0900

    fix: 나한테만 보여주기

--- src/entities/notice/ui/NoticeCard/index.tsx
+++ src/entities/notice/ui/NoticeCard/index.tsx
@@ -2,13 +2,13 @@
 
 import { Button } from '@/shared/components/ui/button';
 import { Notices } from '@/shared/types/noticeType';
-import { Delete, Trash, Trash2 } from 'lucide-react';
+import { Trash2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useCallback } from 'react';
 import { deletePost } from '../../api/deletePost';
 import { cn } from '@/shared/lib/utils';
 
-export default function NoticeCard({ title, id, content }: Notices) {
+export default function NoticeCard({ title, id, content, isMe }: Notices) {
   const R = useRouter();
   const handleClick = useCallback(() => {
     R.push('/detail/' + id);
@@ -26,9 +26,11 @@ export default function NoticeCard({ title, id, content }: Notices) {
         <h3 className={cn('text-titleSmall')}>{title}</h3>
         <p className={cn('text-gray-300')}>{content}</p>
       </div>
-      <Button onClick={handleDelete} variant="outline">
-        <Trash2 color="#DF454A" />
-      </Button>
+      {isMe && (
+        <Button onClick={handleDelete} variant="outline">
+          <Trash2 color="#DF454A" />
+        </Button>
+      )}
     </article>
   );
 }
