ef13e720f8b6a276cd8254e2a0de9b8901108400
bae080311 <s24024@gsm.hs.kr>
2025-07-25 20:59:37 +0900

    feat:  toast message

--- src/entities/notice/api/deletePost.ts
+++ src/entities/notice/api/deletePost.ts
@@ -1,10 +1,13 @@
 import { instance } from '@/shared/lib/axios';
+import { toast } from 'sonner';
 
 export const deletePost = (id: string) => {
   try {
     const res = instance.delete('notice/' + id);
+    toast.success('게시글이 삭제되었습니다');
     return res;
   } catch (error) {
+    toast.error('게시글 삭제 실패했습니다');
     throw error;
   }
 };
