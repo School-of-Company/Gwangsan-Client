a5c85299e67814623c9bcb174274980198bdb18a
bae080311 <s24024@gsm.hs.kr>
2025-08-02 17:33:02 +0900

    feat: delete account

---
+++ src/shared/api/deleteAccount.ts
@@ -0,0 +1,15 @@
+import { toast } from 'sonner';
+import { instance } from '../lib/axios';
+import { removeCookie } from '../lib/cookies';
+
+export const deleteAccount = async () => {
+  try {
+    instance.delete('member');
+    removeCookie('accessToken');
+    removeCookie('refreshToken');
+    toast.success('회원탈퇴에 성공했습니다');
+  } catch (error) {
+    toast.error('회원탈퇴에 실패했습니다');
+    throw error;
+  }
+};
