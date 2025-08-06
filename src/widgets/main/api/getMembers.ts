09b277250ddc39c7b70a881cd42041f0b3c62d3a
bae080311 <s24024@gsm.hs.kr>
2025-08-07 00:15:01 +0900

    feat: query string

--- src/widgets/main/api/getMembers.ts
+++ src/widgets/main/api/getMembers.ts
@@ -1,8 +1,16 @@
 import { instance } from '@/shared/lib/axios';
 
-export const getMembers = async () => {
+export const getMembers = async (
+  nickname: string | undefined,
+  placeName: string | undefined,
+) => {
   try {
-    const res = await instance.get('/member/all');
+    const res = await instance.get('/member/all', {
+      params: {
+        nickname,
+        placeName,
+      },
+    });
     return res.data;
   } catch (error) {
     throw error;
--- src/widgets/main/model/useGetMembers.ts
+++ src/widgets/main/model/useGetMembers.ts
@@ -2,9 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { getMembers } from '../api/getMembers';
 import { MemberType } from '@/shared/types/memberType';
 
-export const useGetMembers = () => {
+export const useGetMembers = (
+  nickname: string | undefined,
+  placeName: string | undefined,
+) => {
   return useQuery<MemberType[]>({
-    queryKey: ['members'],
-    queryFn: getMembers,
+    queryKey: ['members', nickname, placeName],
+    queryFn: () => getMembers(nickname, placeName),
   });
 };
