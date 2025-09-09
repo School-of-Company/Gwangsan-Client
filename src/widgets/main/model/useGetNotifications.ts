4c6c75374a05f45c0fa65b0720c34c1e88fb91a6
bae080311 <s24024@gsm.hs.kr>
2025-09-09 20:00:39 +0900

    remove: trade logic

--- src/widgets/main/model/useGetNotifications.ts
+++ src/widgets/main/model/useGetNotifications.ts
@@ -1,11 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { getNotifications } from '../api/getNotifications';
-import { Reports, SignUp, Trade } from './alertType';
+import { Reports, SignUp } from './alertType';
 
 export interface NotificationResponse {
   reports: Reports[];
   signUps: SignUp[];
-  trades: Trade[];
 }
 
 export const useGetNotifications = () => {
