ac7f990defab14d1ee20e730b373cc55fc0b8a60
bae080311 <s24024@gsm.hs.kr>
2025-10-05 16:43:51 +0900

    fix: log out

--- src/shared/api/signout.ts
+++ src/shared/api/signout.ts
@@ -1,16 +1,6 @@
-import { toast } from 'sonner';
-import { instance } from '../lib/axios';
 import { removeCookie } from '../lib/cookies';
 
-export const signout = async () => {
-  try {
-    await instance.delete('/auth/signout');
-    toast.success('로그아웃에 성공했습니다');
-  } catch (error) {
-    toast.error('로그아웃에 실패했습니다');
-    throw error;
-  } finally {
-    removeCookie('accessToken');
-    removeCookie('refreshToken');
-  }
+export const signout = () => {
+  removeCookie('accessToken');
+  removeCookie('refreshToken');
 };
