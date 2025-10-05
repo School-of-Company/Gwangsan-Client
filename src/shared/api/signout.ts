a94cb2a0d502105444c41a11da86fa4debbceed1
bae080311 <s24024@gsm.hs.kr>
2025-10-05 16:45:39 +0900

    feat: redirect

--- src/shared/api/signout.ts
+++ src/shared/api/signout.ts
@@ -1,6 +1,8 @@
+import { redirect } from 'next/navigation';
 import { removeCookie } from '../lib/cookies';
 
 export const signout = () => {
   removeCookie('accessToken');
   removeCookie('refreshToken');
+  window.location.href = '/signin';
 };
