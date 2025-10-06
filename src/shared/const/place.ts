721284e60a6c702090fac352e983ae5172e4d70a
bae080311 <s24024@gsm.hs.kr>
2025-10-06 17:17:16 +0900

    feat: all place

--- src/shared/const/place.ts
+++ src/shared/const/place.ts
@@ -28,3 +28,30 @@ export const headOptions = Object.entries(HEAD).map(([value, label]) => ({
   value,
   label,
 }));
+
+export const ALL_PLACES: Record<number, string> = {
+  1: '수완마을',
+  2: '고실마을',
+  3: '신가',
+  4: '신창',
+  5: '도산',
+  6: '우산',
+  7: '월곡1',
+  8: '첨단2',
+  9: '월곡2',
+  10: '하남',
+  11: '평동',
+  12: '광산구도시재생공동체센터',
+  13: '광산구자원봉사센터',
+  14: '광산구지역사회보장협의체',
+  15: '투게더광산나눔문화센터',
+} as const;
+
+export type AllPlaceType = keyof typeof ALL_PLACES;
+
+export const allPlaceOptions = Object.entries(ALL_PLACES).map(
+  ([value, label]) => ({
+    value,
+    label,
+  }),
+);
