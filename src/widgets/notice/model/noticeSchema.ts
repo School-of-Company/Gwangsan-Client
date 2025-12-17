ea9ea5f4935ae3713b40f5b95a4cd63435174ef4
bae080311 <s24024@gsm.hs.kr>
2025-12-17 13:12:06 +0900

    fix: nullablel

--- src/widgets/notice/model/noticeSchema.ts
+++ src/widgets/notice/model/noticeSchema.ts
@@ -10,7 +10,7 @@ export const NoticeSchema = z.object({
     .min(1, '내용을 입력해주세요.')
     .max(1000, '최대 1000자까지만 입력 가능합니다.'),
   placeId: z.number().int().positive(),
-  imageIds: z.array(z.number().int().positive()),
+  imageIds: z.array(z.number().int().positive()).nullable(),
 });
 
 export type Notice = z.infer<typeof NoticeSchema>;
