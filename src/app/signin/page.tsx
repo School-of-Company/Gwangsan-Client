0f662a0410c1765d49e9e385fb8463c600ec3158
bae080311 <s24024@gsm.hs.kr>
2025-12-17 14:36:23 +0900

    feat: login description

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,4 +1,17 @@
 import { SignInForm } from '@/widgets/auth';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '로그인 | 시민화폐 광산 어드민',
+  description: '광산구 시민화폐 관리자 로그인',
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: 'https://gwangsan.io.kr/signin',
+  },
+};
 
 export default function SignInPage() {
   return (
