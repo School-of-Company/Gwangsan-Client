f13d6c51d6ecb0d852f433008e364f3a1391cca1
bae080311 <s24024@gsm.hs.kr>
2025-12-18 13:30:43 +0900

    feat: use get member

---
+++ src/widgets/profile/model/useGetMember.ts
@@ -0,0 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
+import { getMember } from '../api/getMember';
+import { PlaceValueType } from '@/shared/const/place';
+import { MEMBER_STATUS } from '@/shared/types/memberType';
+import { MemberRole } from '@/shared/const/role';
+
+interface Member {
+  memberId: number;
+  nickname: string;
+  placeName: PlaceValueType;
+  light: number;
+  description: string;
+  specialties: string[];
+  name: string;
+  phoneNumber: string;
+  role: MemberRole;
+  status: MEMBER_STATUS;
+  joinedAt: string;
+  gwangsan: number;
+}
+
+export const useGetMember = (memberId: string) => {
+  return useQuery<Member>({
+    queryKey: ['member', memberId],
+    queryFn: () => getMember(memberId),
+  });
+};
